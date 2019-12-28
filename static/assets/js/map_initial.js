c09141595636f71aec5866112044baaf1d9f631b
shanlee870502 <alicelee870502@gmail.com>
2019-12-28 09:43:21 +0800

    map
    mpaEvent

--- static/assets/js/map_initial.js
+++ static/assets/js/map_initial.js
@@ -7,7 +7,7 @@ function findLocation()
 {
   clearMarkers();
   const address = document.getElementById("address").value;
-  var icon={
+  var icon = {
     url: "https://cdn.pixabay.com/photo/2016/07/21/00/38/pokemon-1531648_960_720.png", // url
     scaledSize: new google.maps.Size(40,60), // scaled size
     origin: new google.maps.Point(0,0), // origin
@@ -27,56 +27,17 @@ function findLocation()
       }));
       const center_lat = results[0].geometry.location.lat();
       const center_lng = results[0].geometry.location.lng();
-      
-      findActivity(center_lat, center_lng);
-      
+
+      const find = document.getElementById("findbutton");
+      find.style.display='inline';
+      find.addEventListener("click",function(){
+        add(center_lat, center_lng);
+      }, false);
+
     } else {
       console.log(status);
     }
   });
-}
-
-function findActivity(center_lat, center_lng)
-{
-  $('#findbutton').fadeIn().css('display','inline-block');
-  $('#findbutton').click(function(){
-    fetch('http://'+ipconfig+'/api/map/location',
-    {
-      method: 'POST',
-      body: JSON.stringify({'lat':center_lat,'lng':center_lng}), // data can be `string` or {object}!
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    })
-    .then(res => res.json())
-    .then((data) => {
-      $('#findbutton').fadeOut().css('display','none');
-      position=[];
-      for(let i =0;i<5;i++)
-      {
-        // console.log(data[i][0]['eventLocation']);
-        // console.log(data[i][0]['eventName']);
-        // console.log(data[i][0]['eventM_B']);
-        // console.log(data[i][0]['eventM_F']);
-        // console.log(data[i][0]['location']);
-        var location = data[i][0]['location'];
-        position.push(location);
-      }
-      console.log(position);
-      add_ActivityToMap(position);
-    })
-    .catch(err => { throw err });
-  });
-}
-
-function add_ActivityToMap(position)
-{
-  clearMarkers();
-  console.log(position.length);
-  for(let i=0;i<position.length;i++)
-  {
-    addMarker(i);
-  }
 }
 
 function clearMarkers() {
@@ -88,14 +49,55 @@ function clearMarkers() {
   markers = [];
 }
 
-function addMarker(e){
-  const random =Math.floor(Math.random()*10)+1;
+function add(center_lat, center_lng)
+{
+  fetch('http://'+ipconfig+'/api/map/location',
+  {
+    method: 'POST',
+    body: JSON.stringify({'lat':center_lat,'lng':center_lng}), // data can be `string` or {object}!
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  .then(res => res.json())
+  .then((data) => {
+    //console.log(position);
+    for(let i =0;i<5;i++)
+    {
+      console.log(data[i][0]['eventLocation']);
+      console.log(data[i][0]['eventName']);
+      console.log(data[i][0]['eventM_B']);
+      console.log(data[i][0]['eventM_F']);
+      console.log(data[i][0]['location']);
+      
+      var location = data[i][0]['location'];
+      position.push(location);
+      
+    }
+    console.log(position);
+  })
+  .catch(err => { throw err });
+
+  setTimeout(function(){ 
+    console.log("position.length: "+position.length);   
+    for (var i = 0; i < position.length; i++) 
+    {
+      addMarker(i);
+    } 
+  }, 3000);
+
+  
+}
+
+function addMarker(e) {
+  const random =Math.floor(Math.random()*5)+1;
   var icon = {
     url: '/static/gps_images/'+random+'.png', // url
     scaledSize: new google.maps.Size(80,80), // scaled size
     origin: new google.maps.Point(0,0), // origin
     anchor: new google.maps.Point(0, 0) // anchor
   };
+
 
   setTimeout(function() {
     markers.push(new google.maps.Marker({
@@ -105,8 +107,19 @@ function addMarker(e){
         },
         map: map,
         icon:icon,
+        //label: position[e].label,
         animation: google.maps.Animation.DROP
       }));
+      // var infowindow = new google.maps.InfoWindow({
+      //   content: '哈摟你好',
+      //   position: {
+      //     lat: position[e].lat,
+      //     lng: position[e].lng
+      //   },
+      //   maxWidth:200,
+      //   pixelOffset: new google.maps.Size(100, -20) 
+      // });
+      // infowindow.open(map,markers);
   }, e * 150);
 }
 
@@ -128,7 +141,9 @@ function initMap()
     }
   });
 
+
   const submit = document.getElementById("submit");
   submit.addEventListener("click",findLocation,false);
 }
+
 window.addEventListener("load",initMap);
